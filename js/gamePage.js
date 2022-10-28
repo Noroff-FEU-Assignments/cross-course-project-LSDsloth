875362df6a3377b6bb727a7ad24a74689c883465
LSDsloth <lst1788@gmail.com>
2022-10-28 04:27:11 +0200

    Changed index and gamepage to fetch from REST API

---
+++ js/gamePage.js
@@ -0,0 +1,54 @@
+const queryString = document.location.search;
+console.log(queryString);
+
+const title = document.querySelector("title");
+
+const params = new URLSearchParams(queryString);
+
+const name = params.get("name");
+
+console.log(name);
+
+const productsUrl =
+  "http://localhost/flower-power/wordpress/wp-json/wc/store/products";
+
+const gameDescription = document.querySelector(".description");
+const gameCoverImg = document.querySelector(".gameCoverImg");
+const gameInfoContainer = document.querySelector(".gameinfo_container");
+const gameTags = document.querySelector(".tags");
+const gameInfoName = document.querySelector(".gameInfoName");
+
+async function wizard() {
+  title.innerHTML += name;
+
+  try {
+    const response = await fetch(productsUrl);
+    const results = await response.json();
+
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].name === name) {
+        console.log(results[i].description);
+        console.log(results[i].tags.length);
+        console.log(results[i].images[0].src);
+        gameDescription.innerHTML = results[i].description;
+
+        gameCoverImg.innerHTML += `
+        <img src="${results[i].images[0].src}" alt="assassin game" />
+        `;
+
+        for (let n = 0; n < results[i].tags.length; n++) {
+          console.log(results[i].tags[n].name);
+          gameTags.innerHTML += `
+        <a href="#">${results[i].tags[n].name}</a>
+        `;
+        }
+
+        gameInfoName.innerHTML = results[i].name;
+      }
+    }
+
+    console.log(results);
+  } catch (error) {}
+}
+
+wizard();
---
+++ js/games.js
@@ -0,0 +1,133 @@
+const productsUrl =
+  "http://localhost/flower-power/wordpress/wp-json/wc/store/products";
+const gamesContainer = document.querySelector(".games_container");
+const allGamesContainer = document.querySelector(".allGames .games_container");
+const upcomingGamesContainer = document.querySelector(
+  ".upcomingGames .games_container"
+);
+const usedGamesContainer = document.querySelector(
+  ".usedGames .games_container"
+);
+
+const loadingIndicator = document.querySelector(".loadingIndicator");
+
+async function getProducts(url) {
+  const response = await fetch(url);
+  const games = await response.json();
+  console.log(games);
+
+  if (!gamesContainer.innerHTML) {
+    loadingIndicator.style.display = "block";
+  } else {
+    loadingIndicator.style.display = "none";
+  }
+
+  for (let i = 0; i < 5; i++) {
+    console.log("ForEach loop: " + games[i].name);
+    allGamesContainer.innerHTML += `
+               <div class="gamecard">
+               <div class="thumbnail_container">
+                 <a href="gamepage.html?name=${games[i].name}">
+                   <img
+                     class="assassin_thumbnail"
+                     src="${games[i].images[0].src}"
+                     alt="${games[i].name}"
+                     loading="lazy"
+                   />
+                 </a>
+               </div>
+               <div class="gameinfo">
+                 <a href="gamepage.html?name=${games[i].name}"><p class="title">${games[i].name}</p></a>
+                 <div class="subinfo">
+                   <div class="price">
+                     <p class="earlier_price"></p>
+                     <p class="current_price">${games[i].prices.sale_price} ${games[i].prices.currency_symbol}</p>
+                   </div>
+                   <div class="rating">
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star-half"></i>
+                   </div>
+                 </div>
+               </div>
+               <button class="add_to_cart_btn" title="Add to cart">
+                 <i class="fa-solid fa-cart-plus add_to_cart_icon"></i>
+               </button>
+             </div>`;
+  }
+
+  for (let i = 0; i < 5; i++) {
+    console.log("ForEach loop: " + games[i].name);
+    upcomingGamesContainer.innerHTML += `
+               <div class="gamecard">
+               <div class="thumbnail_container">
+                 <a href="gamepage.html?name=${games[i].name}">
+                   <img
+                     class="assassin_thumbnail"
+                     src="${games[i].images[0].src}"
+                     alt="${games[i].name}"
+                     loading="lazy"
+                   />
+                 </a>
+               </div>
+               <div class="gameinfo">
+                 <a href="gamepage.html?name=${games[i].name}"><p class="title">${games[i].name}</p></a>
+                 <div class="subinfo">
+                   <div class="price">
+                     <p class="earlier_price"></p>
+                     <p class="current_price">${games[i].prices.sale_price} ${games[i].prices.currency_symbol}</p>
+                   </div>
+                   <div class="rating">
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star-half"></i>
+                   </div>
+                 </div>
+               </div>
+               <button class="add_to_cart_btn" title="Add to cart">
+                 <i class="fa-solid fa-cart-plus add_to_cart_icon"></i>
+               </button>
+             </div>`;
+  }
+
+  for (let i = 0; i < 5; i++) {
+    console.log("ForEach loop: " + games[i].name);
+    usedGamesContainer.innerHTML += `
+               <div class="gamecard">
+               <div class="thumbnail_container">
+                 <a href="gamepage.html?name=${games[i].name}">
+                   <img
+                     class="assassin_thumbnail"
+                     src="${games[i].images[0].src}"
+                     alt="${games[i].name}"
+                     loading="lazy"
+                   />
+                 </a>
+               </div>
+               <div class="gameinfo">
+                 <a href="gamepage.html?name=${games[i].name}"><p class="title">${games[i].name}</p></a>
+                 <div class="subinfo">
+                   <div class="price">
+                     <p class="earlier_price"></p>
+                     <p class="current_price">${games[i].prices.sale_price} ${games[i].prices.currency_symbol}</p>
+                   </div>
+                   <div class="rating">
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star"></i>
+                     <i class="fa-solid fa-star-half"></i>
+                   </div>
+                 </div>
+               </div>
+               <button class="add_to_cart_btn" title="Add to cart">
+                 <i class="fa-solid fa-cart-plus add_to_cart_icon"></i>
+               </button>
+             </div>`;
+  }
+}
+getProducts(productsUrl);
